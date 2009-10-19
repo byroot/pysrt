9f934a476c125c7f6cb919dfb7b48a19fe9bd67b
Jean Boussier <jean.boussier@feedback20.com>
2009-10-19 22:43:43 +0200

    release: 0.1.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except:
     pass
 
 setup(name='pysrt',
-      version='0.1.5',
+      version='0.1.6',
       author='Jean Boussier',
       author_email='jean.boussier@gmail.com',
       packages=['pysrt'],
