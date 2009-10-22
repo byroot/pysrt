fce3a507d6ad7d2bbdfa61d6f4c1e84e59798f71
Jean Boussier <jean.boussier@feedback20.com>
2009-10-22 12:48:15 +0200

    clean: pep8

--- tests/test_srttime.py
+++ tests/test_srttime.py
@@ -3,18 +3,20 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+file_path = os.path.join(os.path.dirname(__file__), '..')
+sys.path.insert(0, os.path.abspath(file_path))
 
 from pysrt import SubRipTime
 
 
 class TestSimpleTime:
+
     def setUp(self):
         self.time = SubRipTime()
-        
+
     def test_default_value(self):
         assert self.time.ordinal == 0
-        
+
     def test_micro_seconds(self):
         self.time.milliseconds = 1
         assert self.time.milliseconds == 1
@@ -30,7 +32,7 @@ class TestSimpleTime:
         assert self.time.seconds == 1
         self.time.seconds += 60
         assert self.time.minutes == 1
-    
+
     def test_minutes(self):
         self.time.minutes = 1
         assert self.time.minutes == 1
@@ -38,7 +40,7 @@ class TestSimpleTime:
         assert self.time.minutes == 1
         self.time.minutes += 60
         assert self.time.hours == 43
-    
+
     def test_hours(self):
         self.time.hours = 1
         assert self.time.hours == 1
@@ -55,10 +57,10 @@ class TestTimeParsing:
         ('04:00:00,000', (4, 0, 0, 0)),
         ('12:34:56,789', (12, 34, 56, 789)),
     )
-    
+
     def test_parsing(self):
         for time_string, time_items in self.KNOWN_VALUES:
-            assert SubRipTime.from_string(time_string) == SubRipTime(*time_items)
+            assert time_string == SubRipTime(*time_items)
 
     def test_serialization(self):
         for time_string, time_items in self.KNOWN_VALUES:
@@ -66,6 +68,7 @@ class TestTimeParsing:
 
 
 class TestCoercing:
+
     def test_from_tuple(self):
         assert (0, 0, 0, 0) == SubRipTime()
         assert (0, 0, 0, 1) == SubRipTime(milliseconds=1)
@@ -80,5 +83,5 @@ class TestCoercing:
         assert dict(seconds=2) == SubRipTime(seconds=2)
         assert dict(minutes=3) == SubRipTime(minutes=3)
         assert dict(hours=4) == SubRipTime(hours=4)
-        assert dict(hours=1, minutes=2, seconds=3, milliseconds=4) == SubRipTime(1, 2, 3, 4)
-        
+        assert dict(hours=1, minutes=2, seconds=3, milliseconds=4) == \
+            SubRipTime(1, 2, 3, 4)
