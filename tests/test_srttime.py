62c757faf16308336a0d201004f16739d3257e9f
Jean Boussier <jean.boussier@feedback20.com>
2009-10-31 15:45:44 +0100

    test: up to 100% coverage for pysrt.srttime

--- tests/test_srttime.py
+++ tests/test_srttime.py
@@ -8,7 +8,7 @@ import unittest
 file_path = os.path.join(os.path.dirname(__file__), '..')
 sys.path.insert(0, os.path.abspath(file_path))
 
-from pysrt import SubRipTime
+from pysrt import SubRipTime, InvalidTimeString
 
 
 class TestSimpleTime(unittest.TestCase):
@@ -49,6 +49,13 @@ class TestSimpleTime(unittest.TestCase):
         self.time.minutes += 42
         self.assertEquals(self.time.hours, 1)
 
+    def test_shifting(self):
+        self.time.shift(1, 1, 1, 1)
+        self.assertEquals(self.time, (1, 1, 1, 1))
+
+    def test_descriptor_from_class(self):
+        self.assertRaises(AttributeError, lambda: SubRipTime.hours)
+
 
 class TestTimeParsing(unittest.TestCase):
     KNOWN_VALUES = (
@@ -67,6 +74,9 @@ class TestTimeParsing(unittest.TestCase):
     def test_serialization(self):
         for time_string, time_items in self.KNOWN_VALUES:
             self.assertEquals(time_string, str(SubRipTime(*time_items)))
+
+    def test_invalid_time_string(self):
+        self.assertRaises(InvalidTimeString, SubRipTime.from_string, 'hello')
 
 
 class TestCoercing(unittest.TestCase):
@@ -89,7 +99,38 @@ class TestCoercing(unittest.TestCase):
             SubRipTime(1, 2, 3, 4))
 
     def test_from_time(self):
-        assert SubRipTime(1, 2, 3, 4) == time(1, 2, 3, 4000)
-        assert SubRipTime(1, 2, 3, 5) >= time(1, 2, 3, 4000)
-        assert SubRipTime(1, 2, 3, 3) <= time(1, 2, 3, 4000)
-        assert SubRipTime(1, 2, 3, 0) != time(1, 2, 3, 4000)
+        time_obj = time(1, 2, 3, 4000)
+        self.assertEquals(SubRipTime(1, 2, 3, 4), time_obj)
+        self.assertTrue(SubRipTime(1, 2, 3, 5) >= time_obj)
+        self.assertTrue(SubRipTime(1, 2, 3, 3) <= time_obj)
+        self.assertTrue(SubRipTime(1, 2, 3, 0) != time_obj)
+        self.assertEquals(SubRipTime(1, 2, 3, 4).to_time(), time_obj)
+        self.assertTrue(SubRipTime(1, 2, 3, 5).to_time() >= time_obj)
+        self.assertTrue(SubRipTime(1, 2, 3, 3).to_time() <= time_obj)
+        self.assertTrue(SubRipTime(1, 2, 3, 0).to_time() != time_obj)
+
+    def test_from_ordinal(self):
+        self.assertEquals(SubRipTime.from_ordinal(3600000), {'hours': 1})
+        self.assertEquals(SubRipTime(1), 3600000)
+
+
+class TestOperators(unittest.TestCase):
+    
+    def setUp(self):
+        self.time = SubRipTime(1, 2, 3, 4)
+
+    def test_add(self):
+        self.assertEquals(self.time + (1, 2, 3, 4), (2, 4, 6, 8))
+
+    def test_iadd(self):
+        print self.time
+        self.time += (1, 2, 3, 4)
+        print self.time
+        self.assertEquals(self.time, (2, 4, 6, 8))
+
+    def test_sub(self):
+        self.assertEquals(self.time - (1, 2, 3, 4), 0)
+
+    def test_isub(self):
+        self.time -= (1, 2, 3, 4)
+        self.assertEquals(self.time, 0)
