c64cfb91a6f9b50b793752320e6fbb87e7a8214b
Jean Boussier <jean.boussier@gmail.com>
2012-06-17 12:29:28 -0400

    release 0.4.2

--- pysrt/__init__.py
+++ pysrt/__init__.py
@@ -3,5 +3,5 @@ from pysrt.srtitem import SubRipItem
 from pysrt.srtfile import SubRipFile
 from pysrt.srtexc import Error, InvalidItem, InvalidTimeString
 
-VERSION = (0, 4, 1)
+VERSION = (0, 4, 2)
 VERSION_STRING = '.'.join(str(i) for i in VERSION)
